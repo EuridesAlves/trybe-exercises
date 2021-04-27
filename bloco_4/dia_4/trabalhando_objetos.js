4ff72b6463cfce6b11c096150125faaadaf85f7b
Eurides <euridesalves@yahoo.com.br>
2021-04-27 18:20:15 -0300

    Exercicio primeiro apos aula

---
+++ bloco_4/dia_4/trabalhando_objetos.js
@@ -0,0 +1,12 @@
+// trabalhando objetos
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  console.log("Bem vinda, " + info.personagem + "!");
+  info.recorrente = "Sim";
+  
+  console.log("Recorrente? "+info.recorrente +".");
+  console.log(info);
