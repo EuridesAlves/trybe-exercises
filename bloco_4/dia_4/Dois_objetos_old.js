756c7a9eef23f1e729eed50fecbea4f97b73def4
Eurides <euridesalves@yahoo.com.br>
2021-04-28 14:00:00 -0300

    terminei exercicios Objetos e For

--- bloco_4/dia_4/Dois_objetos_old.js
+++ bloco_4/dia_4/Dois_objetos_old.js
@@ -5,7 +5,6 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 info.recorrente = "Sim";
-
 let dois ={
   personagem : "Tio Patinhas",
   origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
@@ -13,10 +12,20 @@ let dois ={
 }
 dois.recorrente = "Sim";
 
+let array1 = [];
+let array2 = [];
 for (let indice in info) {
-  if (indice != "recorrente") {
-    console.log(info[indice] + " e " + dois[indice]);
-  } else {
-    console.log("Ambos recorrentes.");
-  }
+  array1.push(info[indice]);
 }
+for (let indice2 in dois) {
+  array2.push(dois[indice2]);  
+}
+
+for (let posicao = 0 ; posicao <= array1.length -1 ; posicao += 1) {
+  
+  }  
+  for (let posicao2 = 0 ; posicao2 <= array2.length -1; posicao2 += 1) {
+    console.log(array1[posicao] + " e " + array2[posicao2]); 
+
+    /* transformar em uma array conjunta */
+  }
