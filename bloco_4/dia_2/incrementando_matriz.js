9c24c10897ebaf5cbbe8162d1cfa9c00635a21bf
Eurides <euridesalves@yahoo.com.br>
2021-04-30 13:59:14 -0300

    enviando para encerrar branch

--- bloco_4/dia_2/incrementando_matriz.js
+++ bloco_4/dia_2/incrementando_matriz.js
@@ -1,8 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let numberx = [];
 
-for (let index = 1; index < numbers.length; index += 1) {
-    /* if (index <)
-     numberx.push(numbers(index))   */ 
+for (let index = 0; index < numbers.length; index += 1) {
+   numberx.push(numbers[index]); 
 }
-console.log(numbers);
+console.log(numberx);
